57f84c772b046afaafdf697b32159fca61a7a27b
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2023-01-13 23:31:37 +0200

    1.3.1

--- src/main/java/me/serbob/toastedemojis/ToastedEmojis.java
+++ src/main/java/me/serbob/toastedemojis/ToastedEmojis.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
